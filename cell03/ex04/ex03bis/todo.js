63132f475abee22595d8592c6af9a09e9d2cfe21
araiva <kizzaraiva@gmail.com>
2022-05-05 19:38:53 +0700

    to easy solution

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,35 +1,19 @@
-// const todo = ['aa', 'bbb', 'ccc'];
-const todo = [];
-
-$('document').ready(() => {
-	renderList();
-})
-
-function renderList() {
-	const id = '#ft_list'
-	$(id).empty();
-	todo.forEach((el, i) => createListElement(`todo${i}`, el).appendTo(id));
-}
-
 function newItem() {
 	let value = prompt("Please enter your new todo");
-	todo.unshift(value);
-	renderList();
+	createListElement(value).prependTo('#ft_list');
 };
 
 function removeItem(event) {
-	const value = event.target.textContent;
+	const target = $(event.currentTarget);
+	const value = target.text();
 	const confirm = window.confirm(`Do you want to remove "${value}"`);
 	if (confirm) {
-		const index = todo.findIndex(el => el === value);
-		todo.splice(index, 1);
-		renderList();
+		target.remove();
 	}
 }
 
-function createListElement(id, value) {
+function createListElement(value) {
 	return $('<li>', {
-		id,
 		class: 'list-item',
 		text: value,
 		click: removeItem,
