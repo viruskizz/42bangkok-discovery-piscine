c8d9fda070c67fe617a0e7dbac863f6cc5a8b078
araiva <kizzaraiva@gmail.com>
2022-05-07 14:47:38 +0700

    refact code cell03

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,14 +1,20 @@
 // const todo = ['aa', 'bbb', 'ccc'];
-const todo = [];
+let todos = [];
+const key = 'todo';
 
 window.onload = () => {
-	if (todo.length > 0) {
+	const savedTodo = getTodos();
+	if (savedTodo && savedTodo.length > 0) {
+		todos = savedTodo;
 		renderList();
 	}
 }
+
 function newItem(event) {
 	let value = prompt("Please enter your new todo");
-	todo.unshift(value);
+	if (!value) { return; }
+	todos.unshift(value);
+	saveTodos();
 	renderList();
 };
 
@@ -16,8 +22,9 @@ function removeItem(event) {
 	const value = event.target.textContent;
 	const confirm = window.confirm(`Do you want to remove "${value}"`);
 	if (confirm) {
-		const index = todo.findIndex(el => el === value);
-		todo.splice(index, 1);
+		const index = todos.findIndex(el => el === value);
+		todos.splice(index, 1);
+		saveTodos();
 		renderList();
 	}
 }
@@ -26,7 +33,7 @@ function renderList() {
 	const idName = 'todo';
 	const elments = [];
 	const list = document.getElementById('ft_list');
-	todo.forEach((el, i) => {
+	todos.forEach((el, i) => {
 		const item = createListElement(idName + i, el);
 		elments.push(item);
 	});
@@ -40,4 +47,18 @@ function createListElement(id, value) {
 	li.addEventListener('click', (event) => removeItem(event));
 	li.textContent = value;
 	return li;
+}
+
+function saveTodos() {
+	const json = JSON.stringify(todos);
+	document.cookie = `${key}=${json};`
+}
+
+function getTodos() {
+	const cookies = document.cookie.split(';');
+	if (!cookies) {
+		return;
+	}
+	const data = cookies.find(c => c.split('=')[0] == key);
+	return data ? JSON.parse(data.split('=')[1]) : undefined;
 }
--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,19 +1,46 @@
+// const todo = ['aa', 'bbb', 'ccc'];
+let todos = [];
+const key = 'todo';
+// const saveTodos = () => Cookies.set(key, JSON.stringify(todos));
+const saveTodos = () => Cookies.set(key, JSON.stringify(todos));
+const getTodos = () => Cookies.get(key) ? JSON.parse(Cookies.get(key)) : undefined;
+
+$('document').ready(() => {
+	const savedTodo = getTodos();
+	if (savedTodo && savedTodo.length > 0) {
+		todos = savedTodo;
+		renderList();
+	}
+})
+
+function renderList() {
+	const id = '#ft_list'
+	$(id).empty();
+	todos.forEach((el, i) => createListElement(`todo${i}`, el).appendTo(id));
+}
+
 function newItem() {
 	let value = prompt("Please enter your new todo");
-	createListElement(value).prependTo('#ft_list');
+	if (!value) { return; }
+	todos.unshift(value);
+	saveTodos();
+	renderList();
 };
 
 function removeItem(event) {
-	const target = $(event.currentTarget);
-	const value = target.text();
+	const value = event.target.textContent;
 	const confirm = window.confirm(`Do you want to remove "${value}"`);
 	if (confirm) {
-		target.remove();
+		const index = todos.findIndex(el => el === value);
+		todos.splice(index, 1);
+		saveTodos();
+		renderList();
 	}
 }
 
-function createListElement(value) {
+function createListElement(id, value) {
 	return $('<li>', {
+		id,
 		class: 'list-item',
 		text: value,
 		click: removeItem,
