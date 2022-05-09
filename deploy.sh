f610b05c34516e73d9588dd853b3d4e4c4d349a0
araiva <kizzaraiva@gmail.com>
2022-05-10 02:30:15 +0700

    release rush to docs

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -z $1];
+then
+    msg="release rush to docs"
+else
+    msg=$1
+fi
+echo "MESSAGE = $msg"
+cp -r rush/* docs/
+git status
+git add rush docs
+git commit -m "$msg"
+git push
